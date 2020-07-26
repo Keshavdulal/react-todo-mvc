a1332ab1efd1680ad4b389a3cdaa0d808c5074a0
keshavdulal <keshav.dulal@gmail.com>
2020-07-26 12:42:25 +0545

    add dynamic greeting message on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+
+
 
 function Header(){
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    const timeStyle={
+        margin: 0,
+        fontSize: 30,
+        padding: 30,
+        background: 'grey',
+        textAlign: 'center',
+    }
+
+    if (hours < 12 ){
+        timeOfDay = 'Morning';
+        timeStyle.color = 'green';
+    }
+    else if( hours >= 12 && hours < 17){
+        timeOfDay ='Afternoon';
+        timeStyle.color = 'purple';
+    }
+    else if( hours >= 17 && hours < 19){
+        timeOfDay ='Evening';
+        timeStyle.color = 'purple';
+    }
+    else{
+        timeOfDay = 'Night';
+        timeStyle.color = 'orange';
+    }
+
+
     return(
         <div>
-            This is a header
+            <h1 style={timeStyle}>
+                Good {timeOfDay +" "+ hours}
+            </h1> 
         </div>
     );
 }
