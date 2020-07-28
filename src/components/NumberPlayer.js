acc7ff85181e3d187a73079c0510e2ba2bff05ef
keshavdulal <keshav.dulal@gmail.com>
2020-07-28 13:44:52 +0545

    add a redundant NumberPlayer comp

---
+++ src/components/NumberPlayer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import randomcolor from 'randomcolor';
+
+export default class NumberPlayer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            count:0,
+            color:""
+        }
+        this.handleIncrementOnClick=this.handleIncrementOnClick.bind(this);
+        this.handleDecrementOnClick=this.handleDecrementOnClick.bind(this);
+    }
+
+    componentDidUpdate(prevProps,prevState){
+        if(prevState.count !== this.state.count){
+            this.setState({color:randomcolor()});
+        }
+    }
+
+    handleIncrementOnClick(){
+        // this.setState(prevState=>{count:prevState.count++});
+        this.setState(prevState => {
+            return {count: prevState.count++}
+        })
+    }
+
+    handleDecrementOnClick(){
+        // this.setState(prevState=>{count:prevState.count--});
+        this.setState(prevState => {
+            return {
+                count: prevState.count--
+            }
+        })
+    }
+
+    render(){
+            return (
+            <div>
+                <h1 style={{color:this.state.color}}>{this.state.count}</h1>
+
+                <button onClick={this.handleIncrementOnClick}>+</button>
+                <button onClick={this.handleDecrementOnClick}>-</button>
+            </div>
+        )
+    }
+}
