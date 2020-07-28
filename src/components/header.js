37a5316933e29a8e5365dd1d2d610dc9919197c0
keshavdulal <keshav.dulal@gmail.com>
2020-07-28 14:08:30 +0545

    master : demo of conditional rendering in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,61 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-
-
-function Header(){
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-    const timeStyle={
-        margin: 0,
-        fontSize: 30,
-        padding: 30,
-        background: '#eeeeee',
-        textAlign: 'center',
+export default class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isLoading:true,
+        }
     }
 
-    if (hours < 12 ){
-        timeOfDay = 'Morning';
-        timeStyle.color = 'green';
-    }
-    else if( hours >= 12 && hours < 17){
-        timeOfDay ='Afternoon';
-        timeStyle.color = 'purple';
-    }
-    else if( hours >= 17 && hours < 19){
-        timeOfDay ='Evening';
-        timeStyle.color = 'purple';
-    }
-    else{
-        timeOfDay = 'Night';
-        timeStyle.color = 'orange';
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({isLoading:false});
+        },1500);
     }
 
+    render(){
+        const date = new Date();
+        const hours = date.getHours();
+        let timeOfDay;
+        const timeStyle={
+            margin: 0,
+            fontSize: 30,
+            padding: 30,
+            background: '#eeeeee',
+            textAlign: 'center',
+        }
 
-    return(
-        <div>
-            <h1 style={timeStyle}>
-                Good {timeOfDay}
-            </h1> 
-        </div>
-    );
+        if (hours < 12 ){
+            timeOfDay = 'Morning';
+            timeStyle.color = 'green';
+        }
+        else if( hours >= 12 && hours < 17){
+            timeOfDay ='Afternoon';
+            timeStyle.color = 'purple';
+        }
+        else if( hours >= 17 && hours < 19){
+            timeOfDay ='Evening';
+            timeStyle.color = 'purple';
+        }
+        else{
+            timeOfDay = 'Night';
+            timeStyle.color = 'orange';
+        }
+
+        return(
+            <div>
+                <h1 style={timeStyle}>
+                    {
+                    this.state.isLoading
+                    ?
+                    `Loading...` 
+                    :
+                    `Good ${timeOfDay}`
+                    }
+                </h1> 
+            </div>
+        );
+    }
 }
-
-export default Header;
